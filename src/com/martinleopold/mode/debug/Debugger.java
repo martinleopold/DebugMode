ce19e4683b5a1dd811bd3103ee1dfac50aff7a58
Martin Leopold <m@martinleopold.com>
2012-05-31 22:21:59 +0200

    Testing critical JDI functions: breakpoint on setup(), inspecting call stack

--- src/com/martinleopold/mode/debug/Debugger.java
+++ src/com/martinleopold/mode/debug/Debugger.java
@@ -1,14 +1,17 @@
 package com.martinleopold.mode.debug;
 
-import com.sun.jdi.VirtualMachine;
-import com.sun.jdi.event.Event;
-import com.sun.jdi.event.EventSet;
+import com.sun.jdi.*;
+import com.sun.jdi.event.*;
+import com.sun.jdi.request.BreakpointRequest;
+import com.sun.jdi.request.ClassPrepareRequest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import processing.app.Sketch;
 import processing.mode.java.JavaBuild;
 
 /**
- * Main controller class for debugging mode.
- * Mainly works with DebugEditor as the corresponding "view".
- * Uses DebugRunner to launch a VM.
+ * Main controller class for debugging mode. Mainly works with DebugEditor as
+ * the corresponding "view". Uses DebugRunner to launch a VM.
  *
  * @author mlg
  */
@@ -16,9 +19,13 @@ public class Debugger implements VMEventListener {
 
     DebugEditor editor; // editor window, acting as main view
     DebugRunner runtime; // the runtime, contains debuggee VM
+    boolean started = false; // debuggee vm has started, VMStartEvent received
+    ThreadReference initialThread; // initial thread of debuggee vm
+    String mainClassName;
 
     /**
      * Construct a Debugger object.
+     *
      * @param editor The Editor that will act as primary view
      */
     Debugger(DebugEditor editor) {
@@ -26,20 +33,26 @@ public class Debugger implements VMEventListener {
     }
 
     /**
-     * Start a debugging session.
-     * Builds the sketch and launches a VM to run it. VM starts suspended.
-     * Should produce a VMStartEvent.
+     * Start a debugging session. Builds the sketch and launches a VM to run it.
+     * VM starts suspended. Should produce a VMStartEvent.
      */
     void startDebug() {
         stopDebug(); // stop any running sessions
 
         try {
-            JavaBuild build = new JavaBuild(editor.getSketch());
+            Sketch sketch = editor.getSketch();
+            JavaBuild build = new JavaBuild(sketch);
 
-            System.out.println("building sketch");
+            System.out.println("building sketch: " + sketch.getName());
             String appletClassName = build.build();
+            System.out.println("class: " + appletClassName);
+            // folder with assembled/preprocessed src
+            System.out.println("build src: " + build.getSrcFolder().getPath());
+            // folder with compiled code (.class files)
+            System.out.println("build bin: " + build.getBinFolder().getPath());
 
             if (appletClassName != null) {
+                mainClassName = appletClassName;
                 System.out.println("init debuggee runtime");
                 runtime = new DebugRunner(build, editor);
                 System.out.println("launching debuggee runtime");
@@ -58,6 +71,11 @@ public class Debugger implements VMEventListener {
                  * @Override public void run() { runtime.launch(false); // this
                  * blocks until finished } }).start(); return runtime;
                  */
+
+
+                // test setting a breakpoint on setup()
+                //Location
+                //vm.eventRequestManager().createBreakpointRequest(null);
             }
         } catch (Exception e) {
             editor.statusError(e);
@@ -67,8 +85,8 @@ public class Debugger implements VMEventListener {
     }
 
     /**
-     * End debugging session.
-     * Stops and disconnects VM. Should produce VMDisconnectEvent.
+     * End debugging session. Stops and disconnects VM. Should produce
+     * VMDisconnectEvent.
      */
     void stopDebug() {
         if (runtime != null) {
@@ -76,11 +94,11 @@ public class Debugger implements VMEventListener {
             runtime.close();
             runtime = null;
         }
+        started = false;
     }
 
     /**
-     * Resume paused debugging session.
-     * Resumes VM.
+     * Resume paused debugging session. Resumes VM.
      */
     void continueDebug() {
         if (isConnected()) {
@@ -89,8 +107,8 @@ public class Debugger implements VMEventListener {
     }
 
     /**
-     * Callback for VM events.
-     * Will be called from another thread. (VMEventReader)
+     * Callback for VM events. Will be called from another thread.
+     * (VMEventReader)
      *
      * @param es Incoming set of events from VM
      */
@@ -98,15 +116,112 @@ public class Debugger implements VMEventListener {
     public void vmEvent(EventSet es) {
         for (Event e : es) {
             System.out.println("VM Event: " + e.toString());
+
+            if (e instanceof VMStartEvent) {
+                initialThread = ((VMStartEvent) e).thread();
+                started = true;
+
+                printStackTrace(initialThread);
+
+                // ref.type of the thread.
+                /*
+                 * ReferenceType rt = initialThread.referenceType();
+                 * System.out.println("ref.type: " + rt);
+                 * System.out.println("name: " + rt.name()); try {
+                 * System.out.println("sourceName: " + rt.sourceName()); } catch
+                 * (AbsentInformationException ex) {
+                 * System.out.println("sourceName: unknown"); }
+                 *
+                 * // get the threads run method for (Method m :
+                 * rt.methodsByName("run")) { System.out.println(m.toString());
+                 * }
+                 */
+
+                /*
+                 * for (ReferenceType rt : runtime.vm().allClasses()) {
+                 * System.out.println(rt); }
+                 *
+                 */
+
+                /*
+                 * List<ReferenceType> mainClasses =
+                 * runtime.vm().classesByName(mainClassName); if
+                 * (mainClasses.size() == 1) { ReferenceType mainClass =
+                 * mainClasses.get(0); System.out.println("ref.type: " +
+                 * mainClass.toString()); System.out.println("name: " +
+                 * mainClass.name()); try { System.out.println("sourceName: " +
+                 * mainClass.sourceName()); } catch (AbsentInformationException
+                 * ex) { System.out.println("sourceName: unknown"); } for
+                 * (Method m : mainClass.methods()) {
+                 * System.out.println(m.toString()); } }
+                 */
+
+                ClassPrepareRequest mainClassPrepare = runtime.vm().eventRequestManager().createClassPrepareRequest();
+                mainClassPrepare.addClassFilter(mainClassName);
+                mainClassPrepare.enable();
+                runtime.vm().resume();
+            } else if (e instanceof ClassPrepareEvent) {
+                ReferenceType rt = ((ClassPrepareEvent) e).referenceType();
+                debugPrint(rt);
+
+                System.out.println("setting breakpoint on setup()");
+                // get setup() location
+                Location setupLocation = rt.methodsByName("setup").get(0).location();
+                BreakpointRequest setupBp = runtime.vm().eventRequestManager().createBreakpointRequest(setupLocation);
+                setupBp.enable();
+                runtime.vm().resume();
+            } else if (e instanceof BreakpointEvent) {
+                System.out.println("stack trace:");
+                printStackTrace(((BreakpointEvent) e).thread());
+            }
         }
     }
 
     /**
-     * Checks whether the debugger is connected to a debuggee VM.
-     * i.e. a debugging session is running.
+     * Checks whether the debugger is connected to a debuggee VM. i.e. a
+     * debugging session is running. VMStartEvent has been received.
+     *
      * @return true if connected to debuggee VM
      */
     boolean isConnected() {
-        return runtime != null && runtime.vm() != null;
+        return started && runtime != null && runtime.vm() != null;
+    }
+
+    /**
+     * Print call stack trace of a thread.
+     * Only works on suspended threads.
+     * @param t suspended thread to print stack trace of
+     */
+    void printStackTrace(ThreadReference t) {
+        try {
+            System.out.println("thread: " + t.name());
+            int i = 0;
+            for (StackFrame f : t.frames()) {
+                Location l = f.location();
+                System.out.println(i++ + ": " + f.toString() + " @ " + l);
+            }
+        } catch (IncompatibleThreadStateException ex) {
+            Logger.getLogger(Debugger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Print debug info about a ReferenceType.
+     * Prints class name, source file name, lists methods.
+     * @param rt the reference type to print out
+     */
+    private void debugPrint(ReferenceType rt) {
+        System.out.println("ref.type: " + rt);
+        System.out.println("name: " + rt.name());
+        try {
+            System.out.println("sourceName: " + rt.sourceName());
+        } catch (AbsentInformationException ex) {
+            System.out.println("sourceName: unknown");
+        }
+        System.out.println("methods:");
+        for (Method m : rt.methods()) {
+            System.out.println(m.toString());
+        }
+        // get the threads run method for (Method m :
     }
 }
